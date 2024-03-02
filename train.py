0008bbe19404ae6c6eb034061cf50a404094bc18
Hem Gosalia <149922474+xxHeisenberg31xx@users.noreply.github.com>
2024-03-02 19:33:19 +0530

    Update train.py

--- train.py
+++ train.py
@@ -128,7 +128,7 @@ def get_or_build_tokenizer(config, ds, lang):
     return tokenizer
 
 def get_ds(config):
-    ds_raw = load_dataset(f"{config['datasource']}", f'{config["lang_src"]}-{config["lang_tgt"]}', split = 'train[]')
+    ds_raw = load_dataset(f"{config['datasource']}", f'{config["lang_src"]}-{config["lang_tgt"]}', split = 'train')
     
     # Build tokenizers
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
